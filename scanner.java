8850ca3dd6539bef5357d65bc531142870b52daa
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-02-13 12:30:54 +0530

    scanner.java

---
+++ scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class GreetingProgram {
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your username: ");
+        String username = scanner.nextLine();  
+      System.out.println("Hello, " + username + "!");   
+       scanner.close();
+    }
+}
