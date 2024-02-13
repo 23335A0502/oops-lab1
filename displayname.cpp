1da011e93813ee67d1fa8deffb2c3f6a53ba8567
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-02-13 16:00:51 +0530

    displayname.cpp

---
+++ displayname.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"enter the username ";
+    cin>>name;
+   // cout<<"Hello "<<name<<endl;
+   cout<<"hello "<<name<<"\n";
+}
+
+
+
