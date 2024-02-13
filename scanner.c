3e67d2510e26e9c48cfc9a3c1fea57c11c306406
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-02-13 12:08:09 +0530

    scanner.c

--- scanner.c
+++ scanner.c
@@ -1,11 +1,11 @@
- import java.util.Scanner;
- class hello
-{
-public static void main(String args[ ])
-{
-System.out.println(“enter your name : ”);
-Scanner input=new Scanner(System.in);
-String name=input.nextLine( );
-System.out.println(“hello” +name);
+import java.util.Scanner;
+
+public class GreetingProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+System.out.print("Please enter your username: ");
+String username = scanner.nextLine();
+System.out.println("Hello, " + username + "!");
+scanner.close();
+    }
 }
-} 
