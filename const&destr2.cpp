863e44cafdadfeb46dd28bbc81e0c73e5ad79b58
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-03-02 22:40:59 +0530

    const&destr2.cpp

---
+++ const&destr2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Student1{
+    private:
+        string collegename;
+        int collegecode;
+        string fullname;
+        double sempercentage;
+    public:
+        Student1(){
+            collegename="MVGR";
+            collegecode=33;
+            cout<<"Constructor called: "<<endl;
+            cout<<collegename<<endl;
+            cout<<collegecode<<endl;
+        }
+        Student1(string fullname,double sempercentage){
+            cout<<"Constructor called: "<<endl;
+            cout<<fullname<<endl;
+            cout<<sempercentage<<endl;
+        }
+};
+int main(){
+    Student1 obj;
+    Student1 obj1=Student1(ravali",75);
+    Student1 obj2=Student1(“geetha",70);
+}
+
