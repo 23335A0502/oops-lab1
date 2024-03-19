ba5eac0e2ebaee6cad98ad8106667dc7eab4ea69
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-03-19 20:49:02 +0530

    Create operatoroverload.cpp

---
+++ operatoroverload.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Operatoroverload{
+private:
+int real;
+int img;
+public:
+Operatoroverload(int r=0,int i=0){
+real=r;
+img=i;
+}
+Operatoroverload operator-(Operatoroverload const& mainobj){
+Operatoroverload funobj; funobj.real=real-mainobj.real; 
+funobj.img=img-mainobj.img; return funobj;
+}
+void display(){
+cout<<endl<<"The Subtraction of complex numbers is :"<<real<<"-"<<img<<"i"<<endl;
+}
+};
+int main(){
+Operatoroverload mainobj(2,3);
+Operatoroverload mainobj1(4,5);
+Operatoroverload sub; sub=mainobj1-mainobj;
+sub.display();
+}
