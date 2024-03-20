9fe1eb80602ae39ad39f1f8e5b67c240a6b16e31
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-03-21 01:13:32 +0530

    Update operatoroverload.cpp

--- operatoroverload.cpp
+++ operatoroverload.cpp
@@ -1,25 +1,37 @@
 #include<iostream>
 using namespace std;
-class Operatoroverload{
-private:
-int real;
-int img;
-public:
-Operatoroverload(int r=0,int i=0){
-real=r;
-img=i;
-}
-Operatoroverload operator-(Operatoroverload const& mainobj){
-Operatoroverload funobj; funobj.real=real-mainobj.real; 
-funobj.img=img-mainobj.img; return funobj;
-}
-void display(){
-cout<<endl<<"The Subtraction of complex numbers is :"<<real<<"-"<<img<<"i"<<endl;
-}
+class Complex{
+    private: 
+    int real;
+    int image;
+    public:
+    Complex(int r=0,int i=0){
+        real=r;
+        image=i;
+    }
+    void result(){
+        cout<<real<<"+i"<<"("<<image<<")"<<endl;
+    }
+    friend Complex operator*(Complex const& c1,Complex const& c2);
+    friend Complex operator+(Complex const& c1,Complex const& c2);
+    friend Complex operator-(Complex const& c1,Complex const& c2);
+    friend Complex operator/(Complex const& c1,Complex const& c2);
 };
+Complex operator*(Complex const& c1,Complex const& c2){
+    return Complex(((c1.real*c2.real)-(c1.image*c2.image)),((c1.real*c2.image)+(c1.image*c2.real)));
+}
+ Complex operator+(Complex const& c1,Complex const& c2){
+    return Complex(c1.real+c2.real,c1.image+c2.image);
+ }
+ Complex operator-(Complex const& c1,Complex const& c2){
+    return Complex(c1.real-c2.real,c1.image-c2.image);
+ }
 int main(){
-Operatoroverload mainobj(2,3);
-Operatoroverload mainobj1(4,5);
-Operatoroverload sub; sub=mainobj1-mainobj;
-sub.display();
+    Complex c1(1,2),c2(2,5);
+    Complex c3=c1*c2;
+    Complex c4=c1+c2;
+    Complex c5=c1-c2;
+    c3.result();
+    c4.result();
+    c5.result();
 }
