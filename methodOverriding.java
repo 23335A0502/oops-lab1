f0b3c98953f491cbfbcad3cedaf1a55a7f9b9a23
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-03-21 01:26:36 +0530

    Update methodOverriding.java

--- methodOverriding.java
+++ methodOverriding.java
@@ -6,12 +6,12 @@ class A{
 class MethodOr extends  A{
 
     void display(String a){
-        System.out.println(a+"BYE");
+        System.out.println(a+" BYE ");
     }
     public static void main(String args[]){
         MethodOr obj=new MethodOr();
-        obj.display("MADHU");
-        obj.display("RANI");
+        obj.display("mummy");
+        obj.display("daddy");
      
     }
 }
