17916c3db14f5147cf928b1ec272edb377c9d276
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-04-30 19:59:56 +0530

    Create ThreadCreation_runnable.java

---
+++ ThreadCreation_runnable.java
@@ -0,0 +1,20 @@
+public class ThreadCreation_runnable implements Runnable{
+    private String name;
+    ThreadCreation_runnable(String name)
+    {
+        this.name=name;
+    }
+    public void run(){
+        System.out.println("Hello "+name);
+    }
+    public static void main(String[] args) {
+        ThreadCreation_runnable t1 = new ThreadCreation_runnable("Ravali");
+        ThreadCreation_runnable t2 = new ThreadCreation_runnable("Madhu");
+        t1.run();
+        t2.run();
+    }
+}
+
+
+
+
